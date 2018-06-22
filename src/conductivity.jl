de4c9771e1e3ba71d5007b0163763d07e9657e1a
Simon Etter <s.etter@warwick.ac.uk>
2018-06-22 16:04:36 +0800

    Second version for evaluation

--- src/conductivity.jl
+++ src/conductivity.jl
@@ -2,8 +2,14 @@ using ApproxTools
 using PyPlot
 using PyCall;
 @pyimport matplotlib.colors as mplcolors;
-using Compat
+using DataStructures
 
+"""
+    rationalfactor(β,η)
+
+One-dimensional function with simple poles at all the singularities
+of the Fermi-Dirac function in the Bernstein ellipse through `1+η`.
+"""
 function rationalfactor(β,η)
     T = promote_type(typeof.((β,η))...)
     return rationalfactor(convert(real(T),β),convert(T,η))
@@ -11,9 +17,16 @@ end
 function rationalfactor(β::T,η::Union{T,Complex{T}}) where {T <: Real}
     b = semiminor(1+η)
     k = nfermipoles(b,β)
-    return x->(x^2+b^2)^k/mapreduce(b->(x^2+b^2),*,π/β*(1:2:2k-1))
+    return x->(x^2+b^2)^k/mapreduce(b->(x^2+b^2),*,one(T),π/β*(1:2:2k-1))
 end
 
+"""
+   approx_conductivity(β,η, npoly, nrat)
+
+Approximate the conductivity function using the ansatz `f(x1,x2) ≈ p(x1,x2) q(x1) q(x2)`
+where `q` is the polynomial interpolant to `rationalfactor(β,η)` and `p` is the
+polynomial interpolant of the remainder.
+"""
 function approx_conductivity(β::Number,η::Number, npoly::Integer,nrat::Integer)
     T = promote_type(typeof.((β,η))...)
     return approx_conductivity(convert(real(T),β),convert(T,η), npoly,nrat)
@@ -33,145 +46,102 @@ function approx_conductivity(
 end
 
 
-struct PeriodicVector{T} <: AbstractVector{T}
-    data::Vector{T}
-end
-PeriodicVector{T}(l::Integer) where {T} = PeriodicVector{T}(Vector{T}(l))
-Base.size(v::PeriodicVector) = size(v.data)
-Base.getindex(v::PeriodicVector,i::Int) = v.data[mod1(i,length(v))]
-Base.setindex!(v::PeriodicVector,val,i::Int) = v.data[mod1(i,length(v))] = val
-
-type CachedRangeIter{Iter,State,Eltype} <: AbstractVector{Eltype}
+mutable struct Stateful{Iter,State}
     iter::Iter
     state::State
-    cache::PeriodicVector{Eltype}
-    offset::Int
-    first::Int
-    last::Int
 end
-function CachedRangeIter(iter,maxlength)
-    state = start(iter)
-    return CachedRangeIter{
-        typeof(iter),
-        typeof(state),
-        eltype(iter)
-    }(
-        iter,
-        state,
-        PeriodicVector{eltype(iter)}(maxlength),
-        1,
-        1,0
-    )
-end
-Base.indices(c::CachedRangeIter) = (c.first:c.last,)
-function Base.getindex(c::CachedRangeIter,i::Int)
-    @assert i in indices(c,1)
-    return c.cache[c.offset+i-c.first]
-end
+"""
+    Stateful(iter)
 
-function movefirst!(c::CachedRangeIter, i::Integer)
-    @assert i >= c.first
-    @assert i <= length(c.iter)
-    df = i - c.first
-    dl = max(0,i-c.last-1)
+A stateful wrapper around the iterable `iter`.
 
-    # Advance iterator
-    for j = 1:dl
-        @assert !done(c.iter,c.state)
-        _,c.state = next(c.iter,c.state)
-    end
+# Example
+```
+julia> a = Stateful(1:5);
 
-    # Free old memory
-    if method_exists(eltype(c), Tuple{})
-        for j = 1:df
-            c.cache[c.offset+j-1] = eltype(c)()
-        end
-    end
+julia> collect(Base.Iterators.take(a,2))
+2-element Array{Int64,1}:
+ 1
+ 2
 
-    # Update indices
-    c.offset += df
-    c.first = i
-    c.last += dl
-    return c
-end
-function movelast!(c::CachedRangeIter, i::Integer)
-    @assert i >= c.last
-    @assert i - c.first < length(c.cache)
-    @assert i <= length(c.iter)
-    dl = i - c.last
-    len = c.last-c.first+1
-
-    # Fill cache
-    for j = 1:dl
-        @assert !done(c.iter,c.state)
-        c.cache[c.offset+len+j-1],c.state = next(c.iter,c.state)
-    end
-
-    # Update indices
-    c.last = i
-    return c
-end
-
-function update!(p, c::CachedRangeIter)
-    df = findfirst(p, c.first:length(c.iter))-1
-    nf = c.first + df
-    dl = findlast(p, max(nf,c.last):min(nf+length(c.cache)-1, length(c.iter)))-1
-    nl = nf > c.last ?  nf + dl : c.last + max(dl,0)
-
-    if df >= 0
-        # If a valid range has been found
-        movefirst!(c, nf)
-        movelast!(c, nl)
-    else
-        # If no valid range has been found
-        movefirst!(c,c.last+1)
-    end
-    return c
-end
+julia> collect(Base.Iterators.take(a,2))
+2-element Array{Int64,1}:
+ 3
+ 4
+```
+"""
+Stateful(iter) = Stateful(iter,start(iter))
+Base.eltype(::Type{Stateful{Iter,State}}) where {Iter,State} = eltype(Iter)
+Base.iteratorsize(::Type{<:Stateful}) = Base.SizeUnknown()
+Base.start(s::Stateful) = ()
+Base.next(s::Stateful, _) = next(s),()
+Base.next(s::Stateful) = ((_,s.state) = next(s.iter,s.state))[1]
+Base.done(s::Stateful, _) = done(s)
+Base.done(s::Stateful) = done(s.iter,s.state)
 
 
-function eval_sparse(f::Semiseparated,H,Da,Db,ε)
-    n = size(H,1)
+"""
+    eval_sparse(f, H, Da, Db, bwidth)
+
+Evaluate the conductivity formula based on the approximation
+`f(x1,x2) ≈ p(x1,x2) q(x1) q(x2)` where `p` and `q` are polynomials
+in Chebyshev basis.
+
+Terms further than `bwidth` away from the diagonal are ignored in the
+expansion of `p`.
+"""
+function eval_sparse(f::Semiseparated,H,Da,Db,bwidth)
+    N = size(H,1)
     p = f.core::LinearCombination
     q = f.factors[1]
     b = p.basis[1]::Chebyshev
     c = p.coefficients
+    n = size(p.coefficients,1)
 
-    @assert size(H) == size(Da) == size(Db) == (n,n)
+    @assert size(H) == size(Da) == size(Db) == (N,N)
     @assert f.factors == (q,q)
     @assert p.basis == (b,b)
 
-    v1 = zeros(n); v1[1] = 1
+    v1 = zeros(N); v1[1] = 1
     v1 = q(H,v1)
     v2 = full(Db[:,1])
     v2 = q(H,v2)
 
-    σ = zero(complex(eltype(H)))
-    Tv1_cache = CachedRangeIter(b(H,v1), 3*findlast(c->abs(c) > ε, @view(c[:,1])))
-    # Tv1_cache = CachedRangeIter(b(H,v1), size(c,1))
-    # movelast!(Tv1_cache, size(c,1))
-    for (i2,Tv2) in enumerate(b(H,v2))
-        update!(i1->abs(c[i1,i2]) > ε, Tv1_cache)
+    Tv1_iter = Stateful(b(H,v1))
+    Tv1_cache = CircularDeque{typeof(v1)}(min(n,2*bwidth+1))
+    for Tv1k in Base.Iterators.take(Tv1_iter, bwidth)
+        push!(Tv1_cache, Tv1k)
+    end
 
-        for (i1,Tv1) in enumerate(IndexLinear(),Tv1_cache)
-            σ += c[i1,i2]*(Tv1'*Da*Tv2)
+    σ = zero(complex(eltype(H)))
+    for (i2,Tv2) in enumerate(b(H,v2))
+        if i2 > bwidth+1
+            shift!(Tv1_cache)
+        end
+        if i2 <= n - bwidth
+            push!(Tv1_cache, next(Tv1_iter))
+        end
+
+        for (i1,Tv1) in enumerate(Tv1_cache)
+            σ += c[max(i2-bwidth,1)+i1-1,i2]*(Tv1'*Da*Tv2)
         end
     end
-    return σ/n
+    return σ/N
 end
 
-# Evaluate the conductivity formula by diagonalising the Hamiltonian
+"""
+    eval_diag(f,H,Da,Db)
+
+Evaluate the conductivity formula via diagonalising the Hamiltonian.
+"""
 function eval_diag(f,H,Da,Db)
-    n = size(H,1)
+    N = size(H,1)
     E,Ψ = eig(full(H))
     D̃a = Ψ'*Da*Ψ
     D̃b = Ψ'*Db[:,1]
     F = grideval(f,(E,E))
-    return 1/n*sum(
-        F[k1,k2] *
-        Ψ[1,k1]*D̃a[k1,k2]*D̃b[k2]
-        for k1 = 1:n, k2 = 1:n
-    )
+    M = Diagonal(Ψ[1,:])*D̃a*Diagonal(D̃b)
+    return dot(vec(M),vec(F))/N
 end
 
 
